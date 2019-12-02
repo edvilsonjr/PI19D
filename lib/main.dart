be521f61cb96e69be4aee3482ff2b3945f7e34c0
sidney.gtelles <pig1238871>
2019-12-02 09:38:09 -0300

    AndroidX

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pi/view/cadastro_chacara.dart';
 import 'package:pi/view/tela_login.dart';
 import 'package:pi/view/cadastro.dart';
 import 'package:pi/view/home.dart';
@@ -11,7 +12,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: TelaLogin(),
+      home: CadChacara(),
       debugShowCheckedModeBanner: false,
         routes: <String, WidgetBuilder>{
           '/TelaLogin': (BuildContext context) => TelaLogin(),
