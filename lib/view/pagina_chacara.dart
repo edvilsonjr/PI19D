4edabaac5cc5e72b732cd7fc9b17a1fc9bf19e2a
MaV19D <ma_m_bassi@hotmail.com>
2019-12-04 12:30:05 -0300

    Continuação da página da chácara

--- lib/view/pagina_chacara.dart
+++ lib/view/pagina_chacara.dart
@@ -28,7 +28,10 @@ class _PaginaChacaraState extends State<PaginaChacara> {
       body: Column(
         //crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Container(child: Image.network(widget.CapaPrincipal[0])),
+          Container(
+              child: widget.CapaPrincipal[0] == null
+                  ? Icon(Icons.photo)
+                  : Image.network(widget.CapaPrincipal[0])),
           Divider(
             color: Colors.black,
             height: 2.0,
@@ -94,36 +97,154 @@ class _PaginaChacaraState extends State<PaginaChacara> {
               ),
             ],
           ),
-         /*---------contatos---------------*/
-         Padding(
-           padding: const EdgeInsets.only(top: 8.0),
-           child: Row(
-             children: <Widget>[
-               Padding(
-                 padding: const EdgeInsets.only(left: 3.0),
-                 child: Icon(FontAwesomeIcons.whatsapp, color: Colors.green,),
-               ),
-               Text(" (19) 9 8765-4321", style: TextStyle(fontWeight: FontWeight.bold),),
-               Padding(
-                 padding: const EdgeInsets.only(left: 13.0),
-                 child: Icon(FontAwesomeIcons.phoneSquare),
-               ),
-               Text(" (19) 36123456", style: TextStyle(fontWeight: FontWeight.bold),),
-             ],
-           ),
-         ),
+          /*---------contatos---------------*/
           Padding(
-            padding: const EdgeInsets.only(left:3.0, top: 4.0),
+            padding: const EdgeInsets.only(top: 8.0),
             child: Row(
               children: <Widget>[
-                Icon(FontAwesomeIcons.envelopeOpen),
                 Padding(
-                  padding: const EdgeInsets.only(left: 4.0),
-                  child: Text("email@email.com.br", style: TextStyle(fontWeight: FontWeight.bold),),
+                  padding: const EdgeInsets.only(left: 5.0),
+                  child: Icon(
+                    FontAwesomeIcons.whatsapp,
+                    color: Colors.green,
+                    size: 16.0,
+                  ),
+                ),
+                Text(
+                  " (19) 9 8765-4321",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 13.0),
+                  child: Icon(FontAwesomeIcons.phoneSquare,
+                  size: 16.0,),
+                ),
+                Text(
+                  " (19) 36123456",
+                  style: TextStyle(fontWeight: FontWeight.bold),
                 ),
               ],
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.only(left: 5.0, top: 4.0),
+            child: Row(
+              children: <Widget>[
+                Icon(FontAwesomeIcons.envelopeOpen,
+                size: 16.0,),
+                Padding(
+                  padding: const EdgeInsets.only(left: 4.0),
+                  child: Text(
+                    "email@email.com.br",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Divider(
+            color: Colors.black,
+            height: 20.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 3.0, bottom: 10.0),
+            child: Text(
+              "Informações Gerais",
+              style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(left: 45.0),
+                child: Icon(
+                  FontAwesomeIcons.bed,
+                  size: 13.0,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Text("Quartos: "),
+              ),
+              Text("3"),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                child: Text("-"),
+              ),
+              Text("Camas: "),
+              Text("5"),
+              Padding(
+                padding: const EdgeInsets.only(left: 45.0),
+                child: Icon(
+                  FontAwesomeIcons.restroom,
+                  size: 13.0,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Text("Banheiros: "),
+              ),
+              Text("4"),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(left: 45.0),
+                  child: Icon(
+                    FontAwesomeIcons.car,
+                    size: 15.0,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Text("Estacionamento"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                  child: Text("-"),
+                ),
+                Text("Capacidade: "),
+                Text("5"),
+                Text(" carros")
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(left: 45.0),
+                  child: Icon(
+                    FontAwesomeIcons.plug,
+                    size: 13.0,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Text("Tomada 110V: "),
+                ),
+                Text("3"),
+
+                Padding(
+                  padding: const EdgeInsets.only(left: 45.0),
+                  child: Icon(
+                    FontAwesomeIcons.plug,
+                    size: 13.0,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Text("Tomada 220V: "),
+                ),
+                Text("4"),
+              ],
+            ),
+          ),
+          Row(),
         ],
       ),
     );
