b8e66ee1a0b337c2f2ebb60e9b2c6193cc77bbc1
MaV19D <ma_m_bassi@hotmail.com>
2019-12-04 10:20:31 -0300

    Design da tela de login

--- lib/view/tela_login.dart
+++ lib/view/tela_login.dart
@@ -12,99 +12,149 @@ class _TelaLoginState extends State<TelaLogin> {
     return Scaffold(
       backgroundColor: Colors.green[100],
       body: SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Image.asset(
-              'imagem/semfundo.png',
-              width: 250,
-              height: 250,
-            ),
-          ),
-          //Center(
-          //      child:
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
-            child: Card(
-              borderOnForeground: true,
-              shape: RoundedRectangleBorder(
-                side: BorderSide(color: Colors.deepOrange, width: 2),
-                borderRadius: BorderRadius.circular(10),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Image.asset(
+                'imagem/semfundo.png',
+                width: 250,
+                height: 250,
               ),
-              color: Colors.white,
-              //decoration: BoxDecoration(border: Border.all(width: 2, color: Colors.black)),
-              //padding: EdgeInsets.all(20),
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 15, horizontal: 40),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(hintText: "Email"),
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: "Senha",
+            ),
+            //Center(
+            //      child:
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Card(
+                borderOnForeground: true,
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Colors.green[900], width: 2),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                color: Colors.white,
+                //decoration: BoxDecoration(border: Border.all(width: 2, color: Colors.black)),
+                //padding: EdgeInsets.all(20),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 12, horizontal: 30),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(hintText: "Email"),
+                        keyboardType: TextInputType.emailAddress,
                       ),
-                      obscureText: true,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text("Esqueci minha senha"),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: RaisedButton(
-                        onPressed: () {
-                          Navigator.of(context).pushNamed('/Home');
-                        },
-                        padding: EdgeInsets.symmetric(horizontal: 98),
-                        color: Colors.deepOrange,
-                        child: Text(
-                          "Login",
-                          style: TextStyle(color: Colors.white),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: "Senha",
+                        ),
+                        obscureText: true,
+                      ),
+                      Padding(
+                          padding: const EdgeInsets.only(top: 3.0),
+                          child: Row(
+                            children: <Widget>[
+                              Text("Esqueci minha senha, "),
+                              GestureDetector(
+                                child: Text(
+                                  "clique aqui.",
+                                  style: TextStyle(
+                                      color: Colors.deepOrange[400],
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                onTap: () {
+                                  return showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        title: Text("Esqueci minha senha"),
+                                        content: Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: <Widget>[
+                                            Text(
+                                                "Informe seu e-mail cadastrado para redefinir sua senha:"),
+                                            TextField(
+                                              decoration: InputDecoration(
+                                                  hintText: "E-mail"),
+                                            ),
+                                          ],
+                                        ),
+                                        actions: <Widget>[
+                                          FlatButton(
+                                              onPressed: () {},
+                                              child: Text(
+                                                "Enviar",
+                                                style: TextStyle(
+                                                    fontSize: 20.0,
+                                                    color:
+                                                        Colors.deepOrange[400]),
+                                              )),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                },
+                              ),
+                            ],
+                            //
+                          )),
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: RaisedButton(
+                          onPressed: () {
+                            Navigator.of(context).pushNamed('/Home');
+                          },
+                          padding: EdgeInsets.symmetric(horizontal: 98),
+                          color: Colors.deepOrange[400],
+                          child: Text(
+                            "Login",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 16.0),
+                          ),
                         ),
                       ),
-                    ),
-                    RaisedButton(
-                      padding: EdgeInsets.symmetric(horizontal: 75),
-                      onPressed: () {
-                        Navigator.of(context).pushNamed('/Cadastro');
-                      },
-                      color: Colors.deepOrange,
-                      child: Text("Cadastrar-se",
-                          style: TextStyle(color: Colors.white)),
-                    ),
-                    Divider(
-                      color: Colors.black,
-                    ),
-                    Text("Ou"),
-                    GoogleSignInButton(
-                      text: "   Login com Google  ",
-                      onPressed: () {
-                        /* ... */
-                      },
-                      darkMode: true, // default: false
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10),
-                      child: FacebookSignInButton(
+                      RaisedButton(
+                        padding: EdgeInsets.symmetric(horizontal: 75),
                         onPressed: () {
-                          // call authentication logic
+                          Navigator.of(context).pushNamed('/Cadastro');
                         },
-                        text: "Login com Facebook",
+                        color: Colors.deepOrange[400],
+                        child: Text("Cadastrar-se",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 16.0)),
                       ),
-                    ),
-                  ],
+                      Divider(
+                        color: Colors.black,
+                        indent: 15.0,
+                        height: 15.0,
+                      ),
+                      Text("Ou"),
+
+                      GoogleSignInButton(
+                        text: "  Login com Google  ",
+                        onPressed: () {
+                          /* ... */
+                        },
+                        darkMode: true, // default: false
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: FacebookSignInButton(
+                          onPressed: () {
+                            // call authentication logic
+                          },
+                          text: "Login com Facebook",
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          //  ),
-        ],
-      ),
+            //  ),
+          ],
+        ),
       ),
     );
   }
